8e7a8941bd6e7d27a1fdd85a2e0b361c0299ffbb
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-24 14:00:40 +0700

    route and NavLink component

--- starter-template/src/App.tsx
+++ starter-template/src/App.tsx
@@ -2,15 +2,22 @@ import MainLayout from 'layouts/MainLayout'
 import About from 'pages/About'
 import Dashboard from 'pages/Dashboard'
 import StaffList from 'pages/StaffList'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <MainLayout>
-        <Dashboard />
-        <About />
-        <StaffList />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/staff' element={<StaffList />} />
+        </Routes>
       </MainLayout>
+
+      {/* <Dashboard />
+      <About />
+      <StaffList /> */}
     </div>
   )
 }
