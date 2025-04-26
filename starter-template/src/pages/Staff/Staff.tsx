307027e07e620190665be6fe4de6c585df3488e0
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-26 10:20:41 +0700

    Nested-Route

--- starter-template/src/pages/Staff/Staff.tsx
+++ starter-template/src/pages/Staff/Staff.tsx
@@ -10,15 +10,27 @@ export default function Staff() {
       <div className='border-b border-gray-200 text-center text-sm font-medium text-gray-500  '>
         <ul className='-mb-px flex flex-wrap'>
           <li className='mr-2'>
-            <NavLink to='/staff/list' className='inline-block rounded-t-lg border-b-2 border-transparent p-4 '>
+            <NavLink
+              to='/staff/list'
+              className={({ isActive }) => `active inline-block rounded-t-lg border-b-2 p-4
+               ${
+                 isActive
+                   ? 'border-blue-600  text-blue-600'
+                   : ' border-transparent  hover:border-gray-300 hover:text-gray-600 '
+               }`}
+            >
               List
             </NavLink>
           </li>
           <li className='mr-2'>
             <NavLink
               to='/staff/add'
-              className={({ isActive }) => `inline-block rounded-t-lg border-b-2
-               ${isActive ? 'border-blue-600 p-4 text-blue-600' : ''} `}
+              className={({ isActive }) => `active inline-block rounded-t-lg border-b-2 p-4
+               ${
+                 isActive
+                   ? 'border-blue-600  text-blue-600'
+                   : ' border-transparent  hover:border-gray-300 hover:text-gray-600 '
+               }`}
               aria-current='page'
             >
               Add
@@ -29,7 +41,7 @@ export default function Staff() {
 
       <Outlet context={{ profile: { name: 'Duoc' } }} />
 
-      <StaffList />
+      {/* <StaffList /> */}
 
       {/* <AddStaff /> */}
     </div>
